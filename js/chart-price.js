28ab878e1f25cbb408ad81d856010707010dc790
yt <yasunaga.takano@gmail.com>
2026-04-18 17:56:34 +0900

    Update chart-price.js

--- js/chart-price.js
+++ js/chart-price.js
@@ -176,6 +176,18 @@ function createPriceChart(priceChart, candleData) {
 
   for (const a of ichimoku.span1) {
     const bValue = spanBMap.get(a.time);
+    
+
+    const JST_OFFSET = 9 * 60 * 60 * 1000;
+    const date = new Date(a.time * 1000 + JST_OFFSET);
+  
+    const y = date.getFullYear();
+    const m = String(date.getMonth() + 1).padStart(2, "0");
+    const d = String(date.getDate()).padStart(2, "0");
+  
+    console.log(`${y}/${m}/${d}：`+ (bValue === undefined));
+    
+                
     if (bValue === undefined) continue;
 
     if (a.value > bValue) {
